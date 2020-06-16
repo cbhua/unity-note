8269cb238d2a52b53eaadb1db98a509e56f857e0
Chuanbo HUA <34462374+chuanbohua@users.noreply.github.com>
2020-06-16 22:48:52 +0800

    Update scripts

---
+++ Scripts/DamageZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    // Only the first frame will call this function
+    void OnTriggerStay2D(Collider2D other) {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if((controller != null) && (controller.health > 0)) {
+            controller.ChangeHealth(-1);
+        }
+    }
+}
---
+++ Scripts/HealthCollectible.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    // Only the first frame will call this function
+    void OnTriggerEnter2D(Collider2D other) {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if((controller != null) && (controller.health < controller.maxHealth)) {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
+    }
+}
